f81e7af7a2a68c5b56763f132866b479729e9678
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-11-28 14:30:20 -0500

    Initial Commit

---
+++ AnimeManager/AnimeManager.h
@@ -0,0 +1,19 @@
+//
+//  AnimeManager.h
+//  AnimeManager
+//
+//  Created by Lucy Zhang on 11/28/17.
+//  Copyright © 2017 Lucy Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimeManager.
+FOUNDATION_EXPORT double AnimeManagerVersionNumber;
+
+//! Project version string for AnimeManager.
+FOUNDATION_EXPORT const unsigned char AnimeManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimeManager/PublicHeader.h>
+
+
